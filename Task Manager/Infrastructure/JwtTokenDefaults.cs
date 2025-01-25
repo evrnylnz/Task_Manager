d9181f4875c653672c948159a7619f26e36afe76
evrnylnz <evrn.ylnz@gmail.com>
2025-01-25 10:08:02 +0300

    fixes

--- Task Manager/Infrastructure/JwtTokenDefaults.cs
+++ Task Manager/Infrastructure/JwtTokenDefaults.cs
@@ -4,7 +4,7 @@
     {
         public const string Issuer = "http://localhost:5156";
         public const string Audience = "http://localhost:5156";
-        public const string Key = "evrenbabacokgizlisifreevrenbabacokgizlisifre";
+        public const string Key = "evrencokgizlisifreevrencokgizlisifre";
         public const int ExpireDate = 5;
     }
 }
